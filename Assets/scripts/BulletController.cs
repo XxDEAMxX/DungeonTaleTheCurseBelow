6b043b5a3c8c8988139eeab886001055d4a14d46
DEAM <daniel.arevalo@uptc.edu.co>
2025-04-30 00:01:15 -0500

    feat: crash bullet enemy

--- Assets/scripts/BulletController.cs
+++ Assets/scripts/BulletController.cs
@@ -7,8 +7,13 @@ using System.Collections;
 public class BulletController : MonoBehaviour
 {
     public float lifeTime = 2f; // Tiempo de vida de la bala
+    private Rigidbody2D rb;
+
+    private Animator animator;
     void Start()
     {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
         StartCoroutine(DeathDelay()); 
     }
 
@@ -26,8 +31,16 @@ public class BulletController : MonoBehaviour
     {
         if (collision.CompareTag("Zombie"))
         {
-            Destroy(gameObject);
+            animator.SetBool("isCrash", true);
+            rb.linearVelocity = Vector2.zero;
+            rb.bodyType = RigidbodyType2D.Static;
         }
+    }
+
+    void OnDestroy()
+    {
+        Destroy(gameObject);
+        
     }
 }
 
